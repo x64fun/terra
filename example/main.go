a97b19b1a8fd01d2594dd2792a6812c6899cd166
thelark <mperial@foxmail.com>
2021-11-28 10:59:52 +0800

    update something.

--- example/main.go
+++ example/main.go
@@ -1,83 +1,28 @@
 package main
 
-// import (
-// 	"encoding/json"
-// 	"log"
+import (
+	"context"
+	"log"
 
-// 	"github.com/google/uuid"
-// 	"github.com/x64fun/terra/example/pb"
-// 	"github.com/x64fun/terra/internal/gotype"
-// 	"google.golang.org/protobuf/proto"
-// 	"google.golang.org/protobuf/types/known/structpb"
-// )
+	"github.com/jmoiron/sqlx"
+	"github.com/x64fun/terra/example/pb/terra/wxwork"
 
-// func init() {
-// 	log.SetFlags(log.Lshortfile)
-// }
-// func main() {
-// 	func() {
-// 		s := &pb.Setting{
-// 			ID: &gotype.UUID{Data: []byte(uuid.New().String())},
-// 		}
-// 		b, err := proto.Marshal(s)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		log.Println(string(b))
-// 		s2 := &pb.Setting{}
-// 		if err := proto.Unmarshal(b, s2); err != nil {
-// 			panic(err)
-// 		}
-// 		log.Println(s2)
-// 	}()
+	// postgreSQL 驱动
+	_ "github.com/lib/pq"
+)
 
-// 	r := &pb.Request{
-// 		Name: "x64fun",
-// 	}
-// 	var err error
-// 	r.Detail, err = structpb.NewStruct(map[string]interface{}{
-// 		"firstName": "John",
-// 		"lastName":  "Smith",
-// 		"isAlive":   true,
-// 		"age":       27,
-// 		"address": map[string]interface{}{
-// 			"streetAddress": "21 2nd Street",
-// 			"city":          "New York",
-// 			"state":         "NY",
-// 			"postalCode":    "10021-3100",
-// 		},
-// 		"phoneNumbers": []interface{}{
-// 			map[string]interface{}{
-// 				"type":   "home",
-// 				"number": "212 555-1234",
-// 			},
-// 			map[string]interface{}{
-// 				"type":   "office",
-// 				"number": "646 555-4567",
-// 			},
-// 		},
-// 		"children": []interface{}{},
-// 		"spouse":   nil,
-// 	})
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	buf, err := json.Marshal(r)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	log.Println(string(buf))
-
-// 	r2 := &pb.Request{}
-// 	jsonBuf := []byte(`{
-// 		"name": "thelark",
-// 		"detail": {
-// 			"hello": "world"
-// 		}
-// 	}`)
-// 	if err := json.Unmarshal(jsonBuf, r2); err != nil {
-// 		panic(err)
-// 	}
-// 	log.Println(r2.GetDetail().AsMap())
-// 	r2.GetDetail().MarshalJSON()
-// }
+func main() {
+	ctx := context.Background()
+	dsn := "host=127.0.0.1 port=5432 user=postgres password=123456 dbname=postgres sslmode=disable"
+	db, _ := sqlx.Connect("postgres", dsn)
+	tx := db.MustBegin()
+	wxwork.DEFAULT_TABLE_PREFIX = `"scrm_wxwork"`
+	list, err := wxwork.GetDepartmentAllList(ctx, tx, nil, wxwork.DepartmentTableColumnID[tx.DriverName()], wxwork.DepartmentTableColumnName[tx.DriverName()])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for _, item := range list {
+		log.Println(item.ID, item.Name)
+	}
+}
